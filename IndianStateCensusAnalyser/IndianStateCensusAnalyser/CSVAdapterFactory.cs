081089c7cb2867ead3b0c719862f3dda0834c4e2
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-07-13 07:12:53 +0530

    [Rajesh] Added : Indian States Census Information

---
+++ IndianStateCensusAnalyser/IndianStateCensusAnalyser/CSVAdapterFactory.cs
@@ -0,0 +1,33 @@
+﻿using IndianStateCensusAnalyser.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStateCensusAnalyser
+{
+    public class CSVAdapterFactory
+    {
+        public Dictionary<string, CensusDTO> LoadCsvData(StateCensusAnalyser.Country country, string csvFilePath, string dataHeaders)
+        {
+            try
+            {
+                switch (country)
+                {
+                    case (StateCensusAnalyser.Country.INDIA):
+                        return new IndianSensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                    //case (CensusAnalyser.Country.US):
+                    //    return new USCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                    default:
+                        throw new CensusAnalyserException("No Such Country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+
+                }
+            }
+            catch (CensusAnalyserException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
