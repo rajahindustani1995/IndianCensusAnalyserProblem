19ea0134b7fc65aaa45b3a713a57f1c2dbb0f780
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-07-18 13:34:12 +0530

    [Rajesh] Added : Test Cases Of Indian States Census problem

--- IndianStateCensusAnalyser/IndianStateCensusAnalyser/DataDAO/PopulationDataDAO.cs
+++ IndianStateCensusAnalyser/IndianStateCensusAnalyser/DataDAO/PopulationDataDAO.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace IndianStateCensusAnalyser.DataDAO
 {
-    internal class PopulationDataDAO
+    public class PopulationDataDAO
     {
+        public string state;
+        public long population;
+        public long area;
+        public long density;
+
+        public PopulationDataDAO(string state, string population, string area, string density)
+        {
+            this.state = state;
+            this.population = Convert.ToUInt32(population);
+            this.area = Convert.ToUInt32(area);
+            this.density = Convert.ToUInt32(density);
+        }
     }
 }
